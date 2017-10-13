daddacb582b815c0fc8eecbc09373be19a56398a
fsx <system_linux@163.com>
2017-10-13 18:00:38 +0800

    Init content

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Post,Category,Tag
+
+class postAdmin(admin.ModelAdmin):
+    list_display = ['title','created_time','modified_time','category','author']
+
+admin.site.register(Category)
+admin.site.register(Post,postAdmin)
+admin.site.register(Tag)
